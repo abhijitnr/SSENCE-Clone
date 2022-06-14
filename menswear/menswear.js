b595b60df0946ca204cd6cac1d0910ff29e1cb7c
TusharKadam7671 <kadamtushar080@gmail.com>
2022-06-14 18:56:16 +0530

    branch created and middle box created

---
+++ menswear/menswear.js
@@ -0,0 +1,296 @@
+let mensData=[
+    {
+      id: 1,
+      imgUrl:
+      'https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221249M140004_1/jil-sander-green-cotton-bucket-hat.jpg ',
+      brand: "Jil Sander",
+      name: "Green Cotton Bucket Hat",
+      price: 440,
+    },
+    {
+      id: 2,
+      imgUrl:
+      'https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221353M191007_1/stussy-black-cotton-trousers.jpg ',
+      brand: "Stüssy",
+      name: "Black Cotton Trousers",
+      price: 145,
+    },
+    {
+      id: 3,
+      imgUrl:
+      ' https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221843M186002_1/032c-blue-destroyed-jeans.jpg',
+      brand: "032c",
+      name: "Blue Destroyed Jeans",
+      price: 370,
+    },
+    {
+      id: 4,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221843M186001_1/032c-blue-the-032c-jeans.jpg ",
+      brand: "032c",
+      name: "Blue 'The 032c' Jeans",
+      price: 210,
+    },
+    {
+      id: 5,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222776M186001_1/alyx-black-blackmeans-edition-jeans.jpg",
+      brand: "1017 ALYX 9SM",
+      name: "Black Blackmeans Edition Jeans",
+      price: 760,
+    },
+    {
+      id: 6,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M195002_1/alexander-mcqueen-white-viscose-blazer.jpg ",
+      brand: "Alexander McQueen",
+      name: "White Viscose Blazer",
+      price: 3245,
+    },
+    {
+      id: 7,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M195001_1/alexander-mcqueen-pink-wool-blazer.jpg",
+      brand: "Alexander McQueen",
+      name: "Pink Wool Blazer",
+      price: 2490,
+    },
+    {
+      id: 8,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M180000_1/alexander-mcqueen-white-workwear-denim-jacket.jpg ",
+      brand: "Alexander McQueen",
+      name: "White Workwear Denim Jacket",
+      price: 1570,
+    },
+    {
+      id: 9,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M180001_1/alexander-mcqueen-black-polyester-jacket.jpg",
+      brand: "Alexander McQueen",
+      name: "Black Polyester Jacket",
+      price: 1100,
+    },
+    {
+      id: 10,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M204000_1/alexander-mcqueen-white-cotton-sweatshirt.jpg",
+      brand: "Alexander McQueen",
+      name: "White Cotton Sweatshirt",
+      price: 590,
+    },
+    {
+      id: 11,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M192009_1/alexander-mcqueen-blue-cotton-shirt.jpg",
+      brand: "Alexander McQueen",
+      name: "Blue Cotton Shirt",
+      price: 1410,
+    },
+    {
+      id: 12,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M192018_1/alexander-mcqueen-blue-graffiti-harness-shirt.jpg ",
+      brand: "Alexander McQueen",
+      name: "Blue Graffiti Harness Shirt",
+      price: 640,
+    },
+    {
+      id: 13,
+      imgUrl:
+        " https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M192001_1/alexander-mcqueen-white-cotton-shirt.jpg",
+      brand: "Alexander McQueen",
+      name: "White Cotton Shirt",
+      price: 850,
+    },
+    {
+      id: 14,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M186002_1/alexander-mcqueen-white-printed-jeans.jpg",
+      brand: "Alexander McQueen",
+      name: "White Printed Jeans",
+      price: 870,
+    },
+    {
+      id: 15,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M191004_1/alexander-mcqueen-navy-recycled-polyester-cargo-pants.jpg",
+      brand: "Alexander McQueen",
+      name: "Navy Recycled Polyester Cargo Pants",
+      price: 1345,
+    },
+    {
+      id: 16,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222259M193002_1/alexander-mcqueen-navy-selvedge-shorts.jpg",
+      brand: "Alexander McQueen",
+      name: "Navy Selvedge Shorts",
+      price: 590,
+    },
+    {
+      id: 17,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221891M212000_1/a-cold-wall-gray-nylon-polo.jpg",
+      brand: "A-COLD-WALL*",
+      name: "Gray Nylon Polo",
+      price: 355,
+    },
+    {
+      id: 18,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/221417M214048_1/dion-lee-navy-harness-back-tank-top.jpg",
+      brand: "Dion Lee",
+      name: "Navy Harness Back Tank Top",
+      price: 265,
+    },
+    {
+      id: 19,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M180006_1/dime-black-hooded-denim-jacket.jpg",
+      brand: "Dime",
+      name: "Black Hooded Denim Jacket",
+      price: 170,
+    },
+    {
+      id: 20,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M180004_1/dime-off-white-and-blue-cotton-jacket.jpg",
+      brand: "Dime",
+      name: "Off-White & Blue Cotton Jacket",
+      price: 155,
+    },
+    {
+      id: 21,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M202012_1/dime-blue-cotton-hoodie.jpg",
+      brand: "Dime",
+      name: "Blue Cotton Hoodie",
+      price: 140,
+    },
+    {
+      id: 22,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M202013_1/dime-gray-cotton-hoodie.jpg",
+      brand: "Dime",
+      name: "Gray Cotton Hoodie",
+      price: 140,
+    },
+    {
+      id: 23,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M202015_1/dime-gray-cotton-hoodie.jpg",
+      brand: "Dime",
+      name: "Gray Cotton Hoodie",
+      price: 140,
+    },
+    {
+      id: 24,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M202017_1/dime-black-bmf-hoodie.jpg",
+      brand: "Dime",
+      name: "Black BMF Hoodie",
+      price: 140,
+    },
+    {
+      id: 25,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M204012_1/dime-gray-cotton-sweatshirt.jpg",
+      brand: "Dime",
+      name: "Gray Cotton Sweatshirt",
+      price: 130,
+    },
+    {
+      id: 26,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M213018_1/dime-green-cotton-long-sleeve-t-shirt.jpg",
+      brand: "Dime",
+      name: "Green Cotton Long Sleeve T-Shirt",
+      price: 100,
+    },
+    {
+      id: 27,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M180005_1/dime-black-cotton-jacket.jpg",
+      brand: "Dime",
+      name: "Black Cotton Jacket",
+      price: 155,
+    },
+    {
+      id: 28,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M192002_1/dime-off-white-cotton-shirt.jpg",
+      brand: "Dime",
+      name: "Off-White Cotton Shirt",
+      price: 140,
+    },
+    {
+      id: 29,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M192003_1/dime-black-cotton-shirt.jpg",
+      brand: "Dime",
+      name: "Black Cotton Shirt",
+      price: 140,
+    },
+    {
+      id: 30,
+      imgUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_320/f_auto,dpr_1.3/222841M213028_1/dime-orange-cotton-t-shirt.jpg",
+      brand: "Dime",
+      name: "Orange Cotton T-Shirt",
+      price: 50,
+    },
+    {
+        id: 31,
+        imgUrl:
+          "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_280/f_auto,dpr_1.3/222841M188004_1/dime-green-cotton-cargo-pants.jpg",
+        brand: "Dime",
+        name: "Green Cotton Cargo Pants",
+        price: 165,
+      },
+      {
+        id: 32,
+        imgUrl:
+          "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_706,w_470/c_scale,h_280/f_auto,dpr_1.3/222841M193005_1/dime-black-and-gray-polyester-shorts.jpg",
+        brand: "Dime",
+        name: "Black & Gray Polyster Shorts",
+        price: 115,
+      },
+  ];
+  
+  
+
+
+  mensData.forEach(function(elem){
+    let box=document.createElement("div");
+
+    let div1=document.createElement("div");
+    let div2=document.createElement("div");
+    let div3=document.createElement("div");
+    let div4=document.createElement("div");
+
+
+    let avatar=document.createElement("img");
+    avatar.src=elem.imgUrl;
+    div1.append(avatar);
+
+    let brand=document.createElement("span");
+    brand.innerText=elem.brand;
+    brand.setAttribute("class","text");
+    div2.append(brand)
+
+    let name=document.createElement("span");
+    name.innerText=elem.name;
+    name.setAttribute("class","text");
+    div3.append(name);
+
+    let price=document.createElement("span");
+    price.innerText=`$${elem.price}`;
+    price.setAttribute("class","text");
+    div4.append(price);
+
+
+
+    box.append(div1,div2,div3,div4);
+    document.querySelector("#box2").append(box);
+
+  })
