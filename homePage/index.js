beb500597cd25e5c742de03a857b5dc6e82263c0
abhijitnr <abhijitbiswas8859@gmail.com>
2022-06-15 12:46:49 +0530

    homePage_folder_Added

---
+++ homePage/index.js
@@ -0,0 +1,21 @@
+// EventListener for MensWear
+document.querySelector("#men").addEventListener("click", mensFunction);
+
+document.querySelector("#menBotton1").addEventListener("click", mensFunction);
+
+document.querySelector("#menBotton2").addEventListener("click", mensFunction);
+
+function mensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for WomensWear
+document.querySelector("#women").addEventListener("click", womensFunction);
+
+document.querySelector("#womenBotton1").addEventListener("click", womensFunction);
+
+document.querySelector("#womenBotton2").addEventListener("click", womensFunction);
+
+function womensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
