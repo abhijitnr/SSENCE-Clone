7e8a7081b99bc3a9884dc24513a9233b13f46a15
Siddharth263 <priyanshulal2213@gmail.com>
2022-06-18 00:20:48 +0530

    all pages updated+navbar added

--- login&signupPage/checkout.js
+++ login&signupPage/checkout.js
@@ -96,3 +96,53 @@ function toOTP() {
     localStorage.setItem("shippingDetails", JSON.stringify(shippingArray));
     window.location.replace("otp.html");
 }
+// javascript functionalaty
+
+// EventListener for womenswear
+document.querySelector("#mens").addEventListener("click", mensFunction);
+function mensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for womenswear
+document.querySelector("#womens").addEventListener("click", womensFunction);
+function womensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for everythingelse
+document.querySelector("#else").addEventListener("click", elseFunction);
+function elseFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for wishlists
+document.querySelector("#wish").addEventListener("click", wishFunction);
+function wishFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for search button
+document.querySelector("#search").addEventListener("click", searchFunction);
+function searchFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for accountLogo
+document.querySelector("#accountLogo").addEventListener("click", accountFunction);
+function accountFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for shopping bag
+document.querySelector("#shoppingLogo").addEventListener("click", shoppingFunction);
+function shoppingFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+
+// EventListener for brand logo
+document.querySelector("#brandLogo").addEventListener("click", brandLogoFunction);
+function brandLogoFunction(){
+    window.location.href = "index.html"
+}
--- login&signupPage/signin.js
+++ login&signupPage/signin.js
@@ -22,3 +22,53 @@ function valData() {
         document.querySelector("#password").value = "";
     }
 }
+// javascript functionalaty
+
+// EventListener for womenswear
+document.querySelector("#mens").addEventListener("click", mensFunction);
+function mensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for womenswear
+document.querySelector("#womens").addEventListener("click", womensFunction);
+function womensFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for everythingelse
+document.querySelector("#else").addEventListener("click", elseFunction);
+function elseFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for wishlists
+document.querySelector("#wish").addEventListener("click", wishFunction);
+function wishFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for search button
+document.querySelector("#search").addEventListener("click", searchFunction);
+function searchFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for accountLogo
+document.querySelector("#accountLogo").addEventListener("click", accountFunction);
+function accountFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+// EventListener for shopping bag
+document.querySelector("#shoppingLogo").addEventListener("click", shoppingFunction);
+function shoppingFunction(){
+    window.location.href = "https://course.masaischool.com/"
+}
+
+
+// EventListener for brand logo
+document.querySelector("#brandLogo").addEventListener("click", brandLogoFunction);
+function brandLogoFunction(){
+    window.location.href = "index.html"
+}
