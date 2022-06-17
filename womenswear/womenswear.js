050ee7b17a1e1ed9565f77a0026b40e8d48e29ac
TusharKadam7671 <kadamtushar080@gmail.com>
2022-06-17 18:47:36 +0530

    all js part done

--- womenswear/womenswear.js
+++ womenswear/womenswear.js
@@ -2656,3 +2656,83 @@ function accessoriesfun(){
 
 display(filteredaccessories);
 }
+
+
+
+
+document.querySelector("#low2high2").addEventListener("click",sortPrice12);
+document.querySelector("#high2low2").addEventListener("click",sortPrice22);
+
+function sortPrice12(){
+    womensData.sort(function(a,b){
+        if(a.price>b.price) return 1;
+        if(a.price<b.price) return -1;
+        return 0;
+    });
+
+   
+    display(womensData);
+   
+}
+
+function sortPrice22(){
+    womensData.sort(function(a,b){
+        if(a.price<b.price) return 1;
+        if(a.price>b.price) return -1;
+        return 0;
+    });
+   
+    display(womensData);
+    
+}
+
+
+document.querySelector("#all2").addEventListener("click",allfun2);
+function allfun2(){
+  window.location.reload();
+};
+
+document.querySelector("#bags2").addEventListener("click",bagsfun2);
+
+function bagsfun2(){
+  let filteredbags=womensData.filter(function(elem){
+    
+    return elem.tag=="bag";
+})
+
+display(filteredbags);
+}
+
+document.querySelector("#clothing2").addEventListener("click",clothingfun2);
+
+function clothingfun2(){
+  let filteredclothing=womensData.filter(function(elem){
+    
+    return elem.tag=="clothing";
+})
+
+display(filteredclothing);
+}
+
+document.querySelector("#shoes2").addEventListener("click",shoesfun2);
+
+function shoesfun2(){
+  let filteredshoes=womensData.filter(function(elem){
+    
+    return elem.tag=="shoes";
+})
+
+display(filteredshoes);
+}
+
+document.querySelector("#accessories2").addEventListener("click",accessoriesfun2);
+
+function accessoriesfun2(){
+  let filteredaccessories=womensData.filter(function(elem){
+    
+    return elem.tag=="accessories";
+})
+
+display(filteredaccessories);
+}
+
